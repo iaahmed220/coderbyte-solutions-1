42c025d7e2ec3b19617591138901c4d354ee7d17
Randy Ferrer <randfer@gmail.com>
2015-09-01 08:58:36 -0400

    Challenge 21 - Counting Minutes I

---
+++ 21-countingMinutesI.js
@@ -0,0 +1,25 @@
+function CountingMinutesI(str) { 
+  var time = str.split(/[:-]+/g);
+  var start = (+time[0] * 60) + +time[1].substr(0,2);
+  var end = (+time[2] * 60) + +time[3].substr(0,2);
+  var duration = 0;
+
+  // Determine if in same 12 hours 
+  // If not we add 12 hours to our end time
+  if(time[1].substr(2,1) != time[3].substr(2,1) ) {
+    end += (12 * 60); 
+  }
+
+  duration = end - start;
+  
+  // If the result is negative
+  // The end time is the next day (we assume this)
+  // The format does not allow for anything else
+  // so add 24 hours
+  // Will be same am or same pm for both times
+  if(duration < 0) {
+    duration += 1440;
+  }
+
+  return duration;       
+}
