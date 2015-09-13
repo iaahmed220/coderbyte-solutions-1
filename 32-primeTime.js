bc3e4545fcba4ec4509c9dc6969c3c1568a271ee
Randy Ferrer <randfer@gmail.com>
2015-09-13 10:32:32 -0400

    Update to challenge 32 - Prime Time

--- 32-primeTime.js
+++ 32-primeTime.js
@@ -1,21 +1,25 @@
 function PrimeTime(num) {
-  'use strict';
+    'use strict';
 
-  // We only need to check up to the sqrt
-  // of the number rounded up
-  var checkTo = Math.ceil(Math.sqrt(num)), 
-      res = true,
-      i;
+    // We only need to check up to the sqrt
+    // of the number rounded up
+    var checkTo = Math.ceil(Math.sqrt(num)), 
+        res = true,
+        i;
 
-  // One is not a prime number
-  if (num < 2) {
-    return false;
-  }
+    // One is not a prime number
+    if (num < 2) {
+      return false;
+    }
 
-  for (i=2; i < checkTo && res == true; i++) {
-      if (num % i === 0) {
-          res = false;
-      }
-  }
-  return res;
+    if (num == 2) {
+      return true;
+    }
+
+    for (i=2; i < checkTo + 1 && res == true; i++) {
+        if (num % i === 0) {
+            res = false;
+        }
+    }
+    return res;
 }
